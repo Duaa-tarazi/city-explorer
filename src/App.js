a02dd03ae6a720b0c4fb6d3024ac6d6c3ba0492d
duaa-tarazi <dst08itm@yahoo.com>
2021-08-01 20:26:18 +0300

    built the basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Form from 'react-bootstrap/Form';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Button from 'react-bootstrap/Button';
+
+class App extends React.Component {
+  getLocationData=async(event)=>{
+    event.preventDefault();
+    let cityName=event.target.city.value;
+    console.log(cityName);
+    let URL=`https://eu1.locationiq.com/v1/search.php?key=pk.3fda50d4c58c160cd474872dbf430d3a&q=Amman&format=json`
+    let axiosreq = await axios.get(URL);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.getLocationData}>
+          <input type='text' placeholder='Enter City' name='city'/>
+          <button type='submit'>Get Location</button>
+        </form>
+        
+      </>
+    )
+  }
 }
-
 export default App;
