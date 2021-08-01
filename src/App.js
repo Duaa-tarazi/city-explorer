2bf94005cd946cdb22205bb00d8f7db875c617c2
duaa-tarazi <dst08itm@yahoo.com>
2021-08-01 22:52:42 +0300

    add the photo of the location map

--- src/App.js
+++ src/App.js
@@ -7,12 +7,28 @@ import axios from 'axios';
 // import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
+constructor(props){
+  super(props);
+this.state={
+  display:'',
+  lon:'',
+  lat:''
+
+}
+}
+
   getLocationData=async(event)=>{
     event.preventDefault();
     let cityName=event.target.city.value;
     console.log(cityName);
-    let URL=`https://eu1.locationiq.com/v1/search.php?key=pk.3fda50d4c58c160cd474872dbf430d3a&q=Amman&format=json`
+    let URL=`https://eu1.locationiq.com/v1/search.php?key=pk.3fda50d4c58c160cd474872dbf430d3a&q=${cityName}&format=json`
     let axiosreq = await axios.get(URL);
+    this.setState({
+      displayName:axiosreq.data[0].display_name,
+    lon: axiosreq.data[0].lon,
+    lat: axiosreq.data[0].lat,
+    showMap:true
+    })
   }
 
   render() {
@@ -23,6 +39,8 @@ class App extends React.Component {
           <input type='text' placeholder='Enter City' name='city'/>
           <button type='submit'>Get Location</button>
         </form>
+        {this.state.displayName}
+        {<img src={`https://maps.locationiq.com/v3/staticmap?key=pk.3fda50d4c58c160cd474872dbf430d3a&center=${this.state.lat},${this.state.lon}`} alt='map'/>}
         
       </>
     )
